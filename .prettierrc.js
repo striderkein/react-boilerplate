e6da9bd779c450a5043a3db5e286e72ef5e4a2a0
shirow ozawa <striderkein@gmail.com>
2021-12-17 17:48:56 +0900

    Dev (#1)
    * refactor:npmをやめてyarnにした
    
    setupスクリプトでyarn使用を強制してることに注目
    
    * feat:コンポーネント格納用のディレクトリを作成した
    
    分類しないとカオスになるので。
    cf.https://zenn.dev/yoshiko/articles/99f8047555f700
    
    * feat:done->npx eslint --init
    
    * refactor:JSXを使用しているファイルの拡張子をjsxに変更した
    
    airbnbルールによりエラーとなるようになった→ もっともだと思うので採用
    
    それにしてもFacebook公式のコードでエラーとなるのはいささかお粗末なのでは…
    
    * feat:done->yarn add -D prettier
    
    * style:.prettierrc.jsを新規作成し既存コードをフォーマットした
    
    特に末尾セミコロンは絶対消したかった。
    
    * feat:整形ルールを追加
    
    * feat:ワークスペース単位で「保存時にフォーマット」を有効化した
    
    * feat:コード整形を行うスクリプトを追加
    
    Co-authored-by: shirow ozawa <striderkein@users.noreply.github.com>

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  // printWidth: 120,
+};
