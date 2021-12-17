4f0a9d81123a270e41c8853f818a6cd2fc1a9815
shirow ozawa <striderkein@users.noreply.github.com>
2021-12-17 13:40:17 +0900

    feat:整形ルールを追加

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   singleQuote: true,
   semi: false,
+  tabWidth: 2,
   trailingComma: 'all',
   bracketSpacing: true,
+  bracketSameLine: false,
   arrowParens: 'avoid',
   // printWidth: 120,
 };
