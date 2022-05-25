dbbb436f93a6e6ef40e2e377b21b8328cf21f644
shirow ozawa <striderkein@users.noreply.github.com>
2022-05-25 13:11:26 +0900

    refactor:拡張子をtsxに変更した

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Header } from 'components/ui/Header'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      {/*
+      <header className="App-header">
+      </header>
+      */}
+      <Header />
+      <body>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <Button /> */}
+      </body>
+    </div>
+  )
+}
+
+export default App
