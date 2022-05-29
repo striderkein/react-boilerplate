2ede967cffc28ba99a3186ad56dc5f8de4f45db5
shirow ozawa <striderkein@users.noreply.github.com>
2022-05-29 22:27:28 +0900

    feat:「my exercise」の各アイテムを仮データ配列から取得し表示できるようにした

--- src/components/ui/MyExercise.tsx
+++ src/components/ui/MyExercise.tsx
@@ -1,34 +1,28 @@
 import type { FC } from 'react'
-// import { BrowserRouter, Switch, Route, Link, NavLink } from "react-router-dom";
-// import { BrowserRouter, Switch, Route, NavLink } from "react-router-dom";
-// import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ExerciseItem } from 'components'
-// import bgBodyRecord from '/images/btn-body-record.jpg'
+
+interface Exercise {
+  type: string,
+  duration: number,
+  calory: number,
+}
 
 export const MyExercise: FC = () => {
+  const data: Exercise[] = [
+    { type: '家事全般', duration: 10, calory: 16 },
+    { type: '家事全般', duration: 20, calory: 26 },
+    { type: '家事全般', duration: 30, calory: 36 },
+    { type: '家事全般', duration: 40, calory: 46 },
+  ]
+
   return (
-    // <BrowserRouter>
     <div className='my-exercise ha-contents'>
       <header className='exercise-header'><span className='exercise-title'>MY EXERCISE</span><span className='exercise-date'>2021.05.21</span></header>
       <main>
         <ul className='my-exercise-list'>
-          <ExerciseItem type='家事全般' duration={10} calory={26} />
-          {/* <li>
-            <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
-            <div className='exercise-calory'>26 kcal</div>
-          </li>
-          <li>
-            <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
-            <div className='exercise-calory'>26 kcal</div>
-          </li>
-          <li>
-            <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
-            <div className='exercise-calory'>26 kcal</div>
-          </li>
-          <li>
-            <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
-            <div className='exercise-calory'>26 kcal</div>
-          </li> */}
+          {data.map((d) => {
+            return <ExerciseItem type={d.type} duration={d.duration} calory={d.calory} />
+          })}
         </ul>
       </main>
     </div>
