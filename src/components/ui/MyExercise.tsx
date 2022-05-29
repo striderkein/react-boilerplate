13c29b9bbb407c509ed423d3c09c2999d3e1d101
shirow ozawa <striderkein@users.noreply.github.com>
2022-05-29 21:48:43 +0900

    refactor:「my exercise」コンポーネントにmainタグを使用するように変更

--- src/components/ui/MyExercise.tsx
+++ src/components/ui/MyExercise.tsx
@@ -10,24 +10,26 @@ export const MyExercise: FC = () => {
     // <BrowserRouter>
     <div className='my-exercise'>
       <header className='exercise-header'><span className='exercise-title'>MY EXERCISE</span><span className='exercise-date'>2021.05.21</span></header>
-      <ul className='my-exercise-list'>
-        <li>
-          <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
-          <div className='exercise-calory'>26 kcal</div>
-        </li>
-        <li>
-          <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
-          <div className='exercise-calory'>26 kcal</div>
-        </li>
-        <li>
-          <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
-          <div className='exercise-calory'>26 kcal</div>
-        </li>
-        <li>
-          <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
-          <div className='exercise-calory'>26 kcal</div>
-        </li>
-      </ul>
+      <main>
+        <ul className='my-exercise-list'>
+          <li>
+            <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
+            <div className='exercise-calory'>26 kcal</div>
+          </li>
+          <li>
+            <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
+            <div className='exercise-calory'>26 kcal</div>
+          </li>
+          <li>
+            <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
+            <div className='exercise-calory'>26 kcal</div>
+          </li>
+          <li>
+            <span className='exercise-type'>家事全般</span><span className='exercise-duration'>10 min</span>
+            <div className='exercise-calory'>26 kcal</div>
+          </li>
+        </ul>
+      </main>
     </div>
   )
 }
