fd3ec806b1c902025187e600e6189539fb984c25
shirow ozawa <striderkein@users.noreply.github.com>
2022-06-01 16:15:53 +0900

    feat:「my diary」にdayjs使用して日付文字列をフォーマットして表示するようにした

--- src/components/ui/DiaryItem.tsx
+++ src/components/ui/DiaryItem.tsx
@@ -1,16 +1,19 @@
 import type { FC } from 'react'
+import dayjs from 'dayjs';
 
 type Props = {
   date: string,
   content: string,
 }
 
+const DATE_FORMAT = 'YYYY.MM.DD HH:mm'
+
 export const DiaryItem: FC<Props> = (props) => {
   const { date, content } = props
 
   return (
-    <li>
-      <div className='diary-date'>{date}</div>
+    <li className="diary-item">
+      <div className='diary-date'>{dayjs(date).format(DATE_FORMAT)}</div>
       <div className='diary-summary'>{content}</div>
       <div className='diary-content'>{content}</div>
     </li>
