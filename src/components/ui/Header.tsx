f10aa9afb451c0c2b5157cddb7dd2b73b7c4a452
shirow ozawa <striderkein@users.noreply.github.com>
2022-06-01 14:56:22 +0900

    style:delete unnecessary comment

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -2,13 +2,6 @@ import type { FC } from 'react'
 import logo from '../../logo.svg'
 import '../../index.css'
 
-/*
-export type IButton = React.DetailedHTMLProps<
-    React.ButtonHTMLAttributes<HTMLButtonElement>,
-    HTMLButtonElement
->
- */
-
 export const Header: FC = () => {
   return (
     <header className='app-header'>
