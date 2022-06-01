aa5a7f4977dc260b1a49e10624fce8edfa2e6140
shirow ozawa <striderkein@users.noreply.github.com>
2022-06-01 16:20:38 +0900

    style:delete unnecessary comment

--- src/components/ui/ButtonContainer.tsx
+++ src/components/ui/ButtonContainer.tsx
@@ -13,25 +13,6 @@ export const ButtonContainer: FC = () => {
       <SquareButton titleEn='BODY RECORD' titleJa='自分のカラダの記録' imagePath='/images/btn-body-record.jpg' />
       <SquareButton titleEn='MY EXERCISE' titleJa='自分の運動の記録' imagePath='/images/btn-my-exercise.jpg' />
       <SquareButton titleEn='MY DIARY' titleJa='自分の日記' imagePath='/images/btn-my-diary.jpg' />
-      {/*
-      <ul>
-        <li>
-          <NavLink to="/input">input</NavLink>
-        </li>
-        <li>
-          <NavLink to="/graph">graph</NavLink>
-        </li>
-      </ul>
-      */}
     </div>
-    // <Switch>
-    //   <Route exact path="/input">
-    //     <Input />
-    //   </Route>
-    //   <Route exact path="/graph">
-    //     <Graph />
-    //   </Route>
-    // </Switch>
-    // </BrowserRouter>
   )
 }
